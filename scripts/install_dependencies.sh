e81099c6306b2cb83f4b1ca19717dadec172badc
mubarakmedtech <160461176+mubarakmedtech@users.noreply.github.com>
2024-05-15 22:05:22 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
+echo "Current working directory: $(pwd)"
+echo "Source path: $(ls -lah /)"
 
 # Navigate to your Flask application directory
 cd /var/www/html/demo-app
+
+echo "Current working directory: $(pwd)"
+echo "Source path: $(ls -lah /)"
 
 # Install Python dependencies
 pip install -r requirements.txt
